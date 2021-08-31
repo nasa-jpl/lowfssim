1f31b48f6d6fb4bfddcc64add86edb7094dfb5dc
Brandon Dube <brandon.dube@jpl.nasa.gov>
2021-08-31 13:05:34 -0700

    setup files, ignore mac DS_Store files

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""LOWFSSim: model of Low Order Wavefront Sensor in Roman CGI."""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup()
